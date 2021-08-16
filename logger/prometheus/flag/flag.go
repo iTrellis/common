2a34dade7ad55f6001d0539fe6a92623c9e1439f
Henry Huang <hhh@rutcode.com>
2021-08-16 17:26:03 +0800

    remove default logfilepath

--- logger/prometheus/flag/flag.go
+++ logger/prometheus/flag/flag.go
@@ -10,8 +10,7 @@ import (
 func AddFlags(a *kingpin.Application, config *prometheus.Config) {
 	a.Flag("log.level", "Only log messages with the given severity or above. One of: [debug, info, warn, error]").
 		Default("info").StringVar(&config.Level)
-	a.Flag("log.file-name", "Path to the log directory.").
-		Default("./log/prometheus.log").StringVar(&config.FileName)
+	a.Flag("log.file-name", "Path to the log directory.").Default("").StringVar(&config.FileName)
 	a.Flag("log.move-file-type", "Move file type.[0-Never move files, 1-Move files every minute, 2-Move files every hour, 3-Move files every minute day]").
 		Default("3").IntVar(&config.MoveFileType)
 	a.Flag("log.max-length", "The size of one file, 0-Undivided file.").
